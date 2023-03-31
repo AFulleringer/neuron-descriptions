218578354b29f05d4de06beebe7def0ce1e26aee
AlexF <alexfulleringer@gmail.com>
2023-03-31 16:24:03 -0400

    Added an argument to skip the exemplar calculations to instead attempt the symlink directly

--- scripts/compute_exemplars.py
+++ scripts/compute_exemplars.py
@@ -178,7 +178,7 @@ if not args.test_symlink:
                                    num_workers=args.num_workers,
                                    **config.exemplars.kwargs)
 
-print('data_dir', data_dir)
-print('results_dir', results_dir)
+print('data_dir: ', data_dir)
+print('results_dir: ', results_dir)
 if not args.no_link:
     data_dir.symlink_to(results_dir, target_is_directory=True)
